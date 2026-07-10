4acb56b24869b707d662864f208771dc64adf101
Roki Turner <roki@0xcaf2.dev>
2026-07-10 13:02:20 +0900

    fix: respond to code update

--- src/page/main.tsx
+++ src/page/main.tsx
@@ -2,15 +2,11 @@ import { render } from "preact"
 import { Shiba11 } from "./shiba11"
 import { PreactRenderer } from "./renderer/preact"
 import { codeSignal } from "./code"
-import { useSignalEffect } from "@preact/signals"
 
 const preact = new PreactRenderer()
 
 function App() {
-  useSignalEffect(() => {
-    const _ = codeSignal.value
-    preact.requestRerun()
-  })
+  preact.requestRerun()
 
   return (
     <>
